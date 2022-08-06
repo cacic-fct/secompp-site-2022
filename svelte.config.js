f9ba18f39f8b6a0d4b61c4838d189d771de3c0f4
Yudi <renanyaguinuma@hotmail.com>
2022-08-05 22:38:48 -0300

    Add adapter-static
    Build for docs.fct server

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,18 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		  }),
+		  prerender: {
+			default: true,
+		  },
+		  paths: {
+			base: dev ? '' : '/semanas/secompp',
+		},
 	}
 };
 
