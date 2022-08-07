4d2bbbcd810f0eebf40b60b942b2f7463e53b623
Yudi <renanyaguinuma@hotmail.com>
2022-08-07 16:50:46 -0300

    Tweak footer icons

--- src/routes/index-components/footer.svelte
+++ src/routes/index-components/footer.svelte
@@ -17,24 +17,24 @@
         <img
           class="d-inline-block"
           src={logoCacic}
-          style="width: 150px;margin-right: 20px; margin-bottom: 5px;"
+          style="width: 100px;margin-right: 15px; margin-bottom: 5px;"
           alt="Centro Acadêmico de Ciência da Computação"
-        /><img class="d-inline-block" src={logoEjcomp} style="width: 150px;" alt="Empresa Júnior da Computação" /><br
-        /><img class="d-inline-block" src={logoUnesp} style="width: 200px;margin-right: 20px;" alt="Unesp" /><img
+        /><img class="d-inline-block" src={logoEjcomp} style="width: 100px;" alt="Empresa Júnior da Computação" /><br
+        /><img class="d-inline-block" src={logoUnesp} style="width: 100px;margin-right: 15px;" alt="Unesp" /><img
           class="d-inline-block"
           src={logoDmc}
-          style="width: 100px;"
+          style="width: 70px;"
           alt="Departamento de Matemática e Computação"
         />
-        <p style="text-align: left;font-size: 14px;">
+        <p style="text-align: left;font-size: 14px; font-weight: 200;">
           <strong>PROEC</strong>&nbsp;- Pró-Reitoria de Extensão Universitária e Cultura<br /><strong>CLAE</strong> -
-          Centro Local de Apoio à Extensão<br /><strong>Fundunesp</strong> - Fundação para o Desenvolvimento da Unesp<br
-          />
+          Centro Local de Apoio à Extensão<br /><strong>Fundunesp</strong> - Fundação para o Desenvolvimento
+          da&nbsp;Unesp<br />
         </p>
       </div>
       <div class="col-md-4">
         <h3 class="fs-6 text-white">FCT – Faculdade de Ciências e Tecnologia<br /></h3>
-        <span
+        <span style="font-weight: 200; font-size: 14px;"
           >Universidade Estadual Paulista "Júlio de Mesquita Filho"<br />Rua Roberto Simonsen, 305<br />Presidente
           Prudente - SP<br />19060-900</span
         >
@@ -46,5 +46,13 @@
 <style>
   .footer-row > .col-md-4 {
     margin-top: 20px;
+  }
+
+  footer {
+    margin-bottom: 24px;
+  }
+
+  strong {
+    font-weight: 500;
   }
 </style>
