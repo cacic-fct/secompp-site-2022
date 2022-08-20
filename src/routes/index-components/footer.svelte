8cfd4646547b77531b89c2d022a1bd085d66ea12
Igor-MPereira <igor@gargantas.org>
2022-08-20 08:15:18 -0300

    refactor: style refactor

--- src/routes/index-components/footer.svelte
+++ src/routes/index-components/footer.svelte
@@ -1,72 +1,77 @@
 <script>
-  import logoCacic from "$lib/img/cacic.svg";
-  import logoEjcomp from "$lib/img/ejcomp.svg";
-  import logoUnesp from "$lib/img/unesp.svg";
-  import logoDmc from "$lib/img/dmc.webp";
+	import logoCacic from '$lib/img/cacic.svg';
+	import logoEjcomp from '$lib/img/ejcomp.svg';
+	import logoUnesp from '$lib/img/unesp.svg';
+	import logoDmc from '$lib/img/dmc.webp';
 </script>
 
 <footer class="text-white bg-dark" id="footer">
-  <div class="container">
-    <div class="row footer-row">
-      <div class="col-md-4">
-        <h3 class="fs-6 text-white">Patrocinadores</h3>
-        <!-- TODO: Incluir ícones dos patrocinadores -->
-      </div>
-      <div class="col-md-4">
-        <h3 class="fs-6 text-white">Realização</h3>
-        <img
-          class="d-inline-block"
-          src={logoCacic}
-          style="width: 100px;margin-right: 15px; margin-bottom: 5px;"
-          loading="lazy"
-          alt="Centro Acadêmico de Ciência da Computação"
-        /><img
-          class="d-inline-block"
-          src={logoEjcomp}
-          style="width: 100px;"
-          loading="lazy"
-          alt="Empresa Júnior da Computação"
-        /><br /><img
-          class="d-inline-block"
-          src={logoUnesp}
-          style="width: 100px;margin-right: 15px;"
-          loading="lazy"
-          alt="Unesp"
-        /><img
-          class="d-inline-block"
-          src={logoDmc}
-          style="width: 70px;"
-          loading="lazy"
-          alt="Departamento de Matemática e Computação"
-        />
-        <p style="text-align: left;font-size: 14px; font-weight: 200;">
-          <strong>PROEC</strong>&nbsp;- Pró-Reitoria de Extensão Universitária e Cultura<br /><strong>CLAE</strong> -
-          Centro Local de Apoio à Extensão<br /><strong>Fundunesp</strong> - Fundação para o Desenvolvimento
-          da&nbsp;Unesp<br />
-        </p>
-      </div>
-      <div class="col-md-4">
-        <h3 class="fs-6 text-white">FCT – Faculdade de Ciências e Tecnologia<br /></h3>
-        <span style="font-weight: 200; font-size: 14px;"
-          >Universidade Estadual Paulista "Júlio de Mesquita Filho"<br />Rua Roberto Simonsen, 305<br />Presidente
-          Prudente - SP<br />19060-900</span
-        >
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row footer-row">
+			<div class="col-md-4">
+				<h3 class="fs-6 text-white">Patrocinadores</h3>
+				<!-- TODO: Incluir ícones dos patrocinadores -->
+			</div>
+			<div class="col-md-4">
+				<h3 class="fs-6 text-white">Realização</h3>
+				<img
+					class="d-inline-block img-100 mr-2 mb-1"
+					src={logoCacic}
+					style="margin-right: 15px;"
+					loading="lazy"
+					alt="Centro Acadêmico de Ciência da Computação" />
+				<img
+					class="d-inline-block img-100"
+					src={logoEjcomp}
+					loading="lazy"
+					alt="Empresa Júnior da Computação" />
+				<br />
+				<img
+					class="d-inline-block img-100"
+					src={logoUnesp}
+					style="margin-right: 15px;"
+					loading="lazy"
+					alt="Unesp" />
+				<img
+					class="d-inline-block img-70"
+					src={logoDmc}
+					loading="lazy"
+					alt="Departamento de Matemática e Computação" />
+				<p class="text-start fs-light" style="font-size: 14px">
+					<b>PROEC</b>&nbsp;- Pró-Reitoria de Extensão Universitária e Cultura<br />
+					<b>CLAE</b> - Centro Local de Apoio à Extensão<br />
+					<b>Fundunesp</b> - Fundação para o Desenvolvimento da&nbsp;Unesp<br />
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h3 class="fs-6 text-white">
+					FCT – Faculdade de Ciências e Tecnologia<br />
+				</h3>
+				<span style="font-size: 14px;" class="fw-light">
+					Universidade Estadual Paulista "Júlio de Mesquita Filho"<br />
+					Rua Roberto Simonsen, 305<br />
+					Presidente Prudente - SP<br />
+					19060-900
+				</span>
+			</div>
+		</div>
+	</div>
 </footer>
 
-<style>
-  .footer-row > .col-md-4 {
-    margin-top: 20px;
-  }
-
-  footer {
-    margin-bottom: 24px;
-    margin-top: 32px;
-  }
-
-  strong {
-    font-weight: 500;
-  }
+<style lang="sass">
+	.footer-row > .col-md-4 
+		margin-top: 20px
+	
+	footer 
+		margin-bottom: 24px
+		margin-top: 32px
+	
+	b 
+		font-weight: 500
+	
+	.img-100 
+		width: 100px
+	
+	.img-70 
+		width: 70px
 </style>
--- src/routes/index-components/info.svelte
+++ src/routes/index-components/info.svelte
@@ -1,62 +1,61 @@
 <div class="container">
-  <div class="row">
-    <div class="col-md-6">
-      <h1>Localização</h1>
-      <div class="map-container">
-        <iframe
-          title="Map"
-          width="600"
-          height="450"
-          style="border: 0"
-          loading="lazy"
-          allowfullscreen
-          referrerpolicy="no-referrer-when-downgrade"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCVY40T87ywPBvI98OCCNHrivvLlZM2n6k&region=BR&language=pt-BR&q=Faculdade de Ciências e Tecnologia - Câmpus de Presidente Prudente"
-        />
-      </div>
-    </div>
-    <div class="col-md-6">
-      <h1>Inscrições</h1>
-      <div class="table-responsive">
-        <table class="table">
-          <thead>
-            <tr>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Estudantes da Unesp</td>
-              <td aria-label="35 reais">R$ 35,00</td>
-            </tr>
-            <tr>
-              <td>Estudantes de outras instituições</td>
-              <td aria-label="35 reais">R$ 35,00</td>
-            </tr>
-            <tr>
-              <td>Professores e profissionais</td>
-              <td aria-label="50 reais">R$ 50,00</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+	<div class="row">
+		<div class="col-md-6">
+			<h1>Localização</h1>
+			<div class="map-container">
+				<iframe
+					title="Map"
+					width="600"
+					height="450"
+					style="border: 0"
+					loading="lazy"
+					allowfullscreen
+					referrerpolicy="no-referrer-when-downgrade"
+					src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCVY40T87ywPBvI98OCCNHrivvLlZM2n6k&region=BR&language=pt-BR&q=Faculdade de Ciências e Tecnologia - Câmpus de Presidente Prudente" />
+			</div>
+		</div>
+		<div class="col-md-6">
+			<h1>Inscrições</h1>
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
+						<tr>
+							<th />
+							<th />
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Estudantes da Unesp</td>
+							<td aria-label="35 reais">R$ 35,00</td>
+						</tr>
+						<tr>
+							<td>Estudantes de outras instituições</td>
+							<td aria-label="35 reais">R$ 35,00</td>
+						</tr>
+						<tr>
+							<td>Professores e profissionais</td>
+							<td aria-label="50 reais">R$ 50,00</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-  .map-container {
-    overflow: hidden;
-    padding-bottom: 56.25%;
-    position: relative;
-    height: 0;
-    iframe {
-      left: 0;
-      top: 0;
-      height: 100%;
-      width: 100%;
-      position: absolute;
-    }
-  }
+	.map-container {
+		overflow: hidden;
+		padding-bottom: 56.25%;
+		position: relative;
+		height: 0;
+		iframe {
+			left: 0;
+			top: 0;
+			height: 100%;
+			width: 100%;
+			position: absolute;
+		}
+	}
 </style>
