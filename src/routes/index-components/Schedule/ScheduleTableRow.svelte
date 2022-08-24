52fb2bff9578793570c9092f6c2f066a55771d9d
Igor-MPereira <igor@gargantas.org>
2022-08-24 19:30:00 -0300

    refactor

--- src/routes/index-components/Schedule/ScheduleTableRow.svelte
+++ src/routes/index-components/Schedule/ScheduleTableRow.svelte
@@ -7,6 +7,7 @@
     getEventType,
     type ScheduleEventRow,
   } from './ScheduleData';
+  import ScheduleTableCell from './ScheduleTableCell.svelte';
 
   export let schedule: ScheduleEventRow;
 
@@ -34,17 +35,7 @@
   </td>
   {#each schedule as event}
     {#if event}
-      <td tabindex="0" role="button" class="shadow-sm">
-        <div class="d-block">
-          <span class="fw-bold">{event.eventName}</span>
-          {#if event.shortDescription}
-            <br />
-            <span class="smaller lh-sm">
-              {event.shortDescription}
-            </span>
-          {/if}
-        </div>
-      </td>
+      <ScheduleTableCell {event} />
     {:else}
       <td aria-hidden="true" aria-disabled="true" />
     {/if}
@@ -52,9 +43,7 @@
 </tr>
 
 <style lang="scss">
-  @use './styles' as *;
-  $transition-duration: 0.3s;
-  $border-radius: 6px;
+  @use './styles';
 
   .table-icon {
     filter: invert(100%) sepia(64%) saturate(532%) hue-rotate(353deg)
@@ -68,21 +57,13 @@
     border-width: 0;
     outline: 0 solid transparent;
 
-    span {
-      transition: $transition-duration;
-      &:not(:first-child) {
-        color: var(--bs-gray-500);
-      }
-    }
-
     &:first-child {
-      @include cell-first-child();
+      @include styles.cell-first-child();
       background-color: #000;
       border-width: 1px;
       border-left-width: 0;
-      border-top-right-radius: $border-radius;
-      border-bottom-right-radius: $border-radius;
-      // border-color: $primary-color;
+      border-top-right-radius: styles.$border-radius;
+      border-bottom-right-radius: styles.$border-radius;
 
       div {
         @media (min-width: 574px) {
@@ -104,34 +85,12 @@
 
     &:not(:first-child) {
       min-width: 200px;
-      color: $body-color;
+      color: styles.$body-color;
     }
 
     &[aria-disabled] {
       min-width: 120px;
       border-width: 0;
-    }
-
-    &:not([aria-disabled], :first-child) {
-      padding: 8px;
-      border-radius: $border-radius;
-      transition: $transition-duration;
-      box-shadow: 0 0.125rem 0.25rem hsla(0, 3%, 60%, 0.336) !important;
-
-      &:hover,
-      &:focus {
-        background: rgba(52, 58, 64, 0.4);
-        outline: 1px solid $primary-color;
-        box-shadow: 0 0.125rem 0.25rem rgb(253 189 0 / 45%) !important;
-
-        span {
-          color: $body-color;
-
-          &:first-child {
-            color: $primary-color;
-          }
-        }
-      }
     }
   }
 </style>
