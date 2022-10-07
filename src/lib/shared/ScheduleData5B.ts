c1ee465dae70ce92a9696f68d4702b50e4e789e3
Willian Murayama <willianyos13@gmail.com>
2022-10-07 00:30:00 -0300

    Adição dos eventos dos laboratórios 5B e 6B

--- src/lib/shared/ScheduleData5B.ts
+++ src/lib/shared/ScheduleData5B.ts
@@ -3,14 +3,13 @@ import { createEvent } from './ScheduleEventUtils';
 
 const ScheduleData: ScheduleEventList = [
   createEvent(
-    // DAY ONE RÉSUMÉ
     'Introdução ao Rust: a linguagem perfeita?',
-    'Unknown',
+    'Laboratório central 5B',
     0,
     new Date('2022-10-17T14:00'),
-    120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa.',
-    'Desenvolvimento de résumé e prática de entrevista.',
+    240,
+    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto.',
+    'Introdução a linguagem Rust',
     [
       {
         name: 'Daniel Henrique Serezane Pereira',
@@ -27,111 +26,142 @@ const ScheduleData: ScheduleEventList = [
     ]
   ),
   createEvent(
-    // DAY TWO RÉSUMÉ
-    'Job interview: como escrever um résumé e apresentar-se para um emprego ' + 'em língua inglesa.',
-    'Unknown',
+    'Do zero ao bootstrap',
+    'Laboratório central 5B',
     0,
-    new Date('2022-10-19T16:00'),
+    new Date('2022-10-18T14:00'),
     120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se ' +
-      'preparar para entrevistas em língua inglesa.',
-    'Desenvolvimento de résumé e prática de entrevista.',
+    'Este minicurso irá ensinar utilizar o framework Bootstrap',
+    'Este minicurso irá ensinar utilizar o framework Bootstrap',
     [
       {
-        name: 'Paula Tavares Pinto',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
-        email: 'paula.pinto@unesp.br',
+        name: 'Lucas Gaspar Stumpf',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'lg.stumpf@unesp.br',
       },
       {
-        name: 'Maurício Araújo Dias',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
-        email: 'ma.dias@unesp.br',
+        name: 'Manoela Moreno Frattini',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'manoela.frattini@unesp.br',
       },
     ]
   ),
   createEvent(
-    'Introdução ao Rust: a lunguagem perfeita?',
-    'Unknown',
+    'Introdução ao Rust: a linguagem perfeita?',
+    'Laboratório central 5B',
     0,
-    new Date('1970-01-01T00:00'),
-    60,
-    'Este minicurso apresentará a linguagem Rust, junto com o contexto ' +
-      'de sua utilização e suas principais aplicações no mercado e o ' +
-      'desenvolvimento um projeto.',
-    'Abordagem e desenvolvimento de um projeto em Rust.',
+    new Date('2022-10-19T14:00'),
+    240,
+    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto.',
+    'Introdução a linguagem Rust',
     [
       {
         name: 'Daniel Henrique Serezane Pereira',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
         email: 'daniel.serezane@unesp.br',
       },
       {
-        name: 'Gustavo Becelli do Nacimento',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
+        name: 'Gustavo Becelli do Nascimento',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
         email: 'gustavo.becelli@unesp.br',
       },
     ]
   ),
   createEvent(
-    'GENERIC-TITLE-AUTOMATED-TESTING',
-    'Unknown',
+    'Do zero ao bootstrap',
+    'Laboratório central 5B',
     0,
-    new Date('1970-01-01T00:00'),
-    60,
-    'Este minicurso irá apresentar os principais conceitos e práticas ' +
-      'sobre a escrita de testes automatizados de software, abordando ' +
-      'suas vantagens e desafios.',
-    'Abordagem da prática de testes automatizados de software..',
+    new Date('2022-10-20T14:00'),
+    120,
+    'Este minicurso irá ensinar utilizar o framework Bootstrap',
+    'Este minicurso irá ensinar utilizar o framework Bootstrap',
     [
       {
-        name: 'Thiago Aires Geraldo de Almeida',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
-        email: 'thiago.almeida@lambda3.com.br',
+        name: 'Lucas Gaspar Stumpf',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'lg.stumpf@unesp.br',
+      },
+      {
+        name: 'Manoela Moreno Frattini',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'manoela.frattini@unesp.br',
       },
     ]
   ),
   createEvent(
-    'Criando modelo de ML de maneira colaborativa - Uma aplicação com MLflow',
-    'Uknown',
+    'Introdução a gitHub',
+    'Laboratório central 5B',
     0,
-    new Date('2022-10-20T08:00'),
-    240,
-    'Demonstrar o uso colaborativo do MLflow para implantar modelos de ' + 'Machine Learning com Databricks.',
-    'Implantação de modelos de Machine Learning com Databricks.',
+    new Date('2022-10-21T14:00'),
+    150,
+    'Este minicurso irá ensinar como utilizar o GitHub e suas principais funcionalidades',
+    'Introdução a plataforma GitHub',
     [
       {
-        name: 'Téo Calvo',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub' },
-        email: 'teo.bcalvo@gmail.com',
+        name: 'Lucas Gaspar Stumpf',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'lg.stumpf@unesp.br',
+      },
+      {
+        name: 'Manoela Moreno Frattini',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'manoela.frattini@unesp.br',
       },
     ]
   ),
   createEvent(
-    'Sistemas Conversacionais: Oportunidades e Desafios',
+    // DAY ONE RÉSUMÉ
+    'Job interview: como escrever um résumé e apresentar-se para um emprego ' + 'em língua inglesa.',
     'Unknown',
-    1,
-    new Date('2022-10-17T20:00'),
-    60,
-    'Esta palestra abordará alguns dos avanços atuais no design e ' +
-      'desenvolvimento de sistemas conversacionais, tanto quanto ' +
-      'resultados recentes da pesquisa de Claudio.',
-    'Abordagem de sistemas conversacionais após uma década de progresso.',
+    0,
+    new Date('2022-10-18T16:00'),
+    120,
+    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa.',
+    'Desenvolvimento de résumé e prática de entrevista.',
     [
       {
-        name: 'Claudio Pinhanez',
-        description:
-          'Pesquisador da IBM Research, vice-diretor do C4AI (Centro de ' +
-          'Pesquisas em Inteligência Artificial), especialista em inteligência ' +
-          'artificial, interação homem-máquina, sistemas conversacionais ' +
-          'e ciência de serviços.',
-        link: { path: 'https://github.com/', label: 'GitHub' },
-        email: '...',
+        name: 'Paula Tavares Pinto',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'paula.pinto@unesp.br',
+      },
+      {
+        name: 'Maurício Araújo Dias',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'ma.dias@unesp.br',
+      },
+    ]
+  ),
+  createEvent(
+    // DAY ONE RÉSUMÉ
+    'Job interview: como escrever um résumé e apresentar-se para um emprego ' + 'em língua inglesa.',
+    'Unknown',
+    0,
+    new Date('2022-10-20T16:00'),
+    120,
+    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa.',
+    'Desenvolvimento de résumé e prática de entrevista.',
+    [
+      {
+        name: 'Paula Tavares Pinto',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'paula.pinto@unesp.br',
+      },
+      {
+        name: 'Maurício Araújo Dias',
+        description: ' ',
+        // link: { path: 'https://github.com/', label: 'GitHub' },
+        email: 'ma.dias@unesp.br',
       },
     ]
   ),
--- src/lib/shared/ScheduleData6B.ts
+++ src/lib/shared/ScheduleData6B.ts
@@ -2,139 +2,146 @@ import type { ScheduleEventList } from './ScheduleEvent';
 import { createEvent } from './ScheduleEventUtils';
 
 const ScheduleData: ScheduleEventList = [
-  createEvent( // DAY ONE RÉSUMÉ
-    'Job interview: como escrever um résumé e apresentar-se para um emprego '
-    + 'em língua inglesa.',
-    'Unknown',
+  createEvent(
+    'Introdução à Linguagem Python para Análise de Dados',
+    'Lahboratório central 6B',
     0,
-    new Date('2022-10-18T16:00'),
+    new Date('2022-10-18T08:00'),
     120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa.',
-    'Desenvolvimento de résumé e prática de entrevista.',
+    'Introduzir o aluno à sintaxe da Linguagem de Programação Python e suas bibliotecas principais para análise de dados, Pandas e Matplotlib, além do ambiente Jupyter Notebook',
+    'Introdução à linguagem de programação Python',
     [
       {
-        name: 'Paula Tavares Pinto',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'paula.pinto@unesp.br',
+        name: 'Guilherme Eduardo Silva Batalhoti',
+        description: ' ',
+        email: 'guilherme.batalhoti@unesp.br',
       },
       {
-        name: 'Maurício Araújo Dias',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'ma.dias@unesp.br',
-      },
-    ]
-  ),
-  createEvent( // DAY TWO RÉSUMÉ
-    'Job interview: como escrever um résumé e apresentar-se para um emprego '
-    + 'em língua inglesa.',
-    'Unknown',
-    0,
-    new Date('2022-10-19T16:00'),
-    120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se '
-    + 'preparar para entrevistas em língua inglesa.',
-    'Desenvolvimento de résumé e prática de entrevista.',
-    [
-      {
-        name: 'Paula Tavares Pinto',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'paula.pinto@unesp.br',
-      },
-      {
-        name: 'Maurício Araújo Dias',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'ma.dias@unesp.br',
+        name: 'Lucas Vinícius Voltera',
+        description: ' ',
+        email: 'lucas.voltera@unesp.br',
       },
     ]
   ),
   createEvent(
-    'Introdução ao Rust: a lunguagem perfeita?',
-    'Unknown',
+    'Introdução à Linguagem Python para Análise de Dados',
+    'Laboratório central 6B',
     0,
-    new Date('1970-01-01T00:00'),
-    60,
-    'Este minicurso apresentará a linguagem Rust, junto com o contexto '
-    + 'de sua utilização e suas principais aplicações no mercado e o '
-    + 'desenvolvimento um projeto.',
-    'Abordagem e desenvolvimento de um projeto em Rust.',
+    new Date('2022-10-19T08:00'),
+    120,
+    'Introduzir o aluno à sintaxe da Linguagem de Programação Python e suas bibliotecas principais para análise de dados, Pandas e Matplotlib, além do ambiente Jupyter Notebook',
+    'Introdução à linguagem de programação Python',
     [
       {
-        name: 'Daniel Henrique Serezane Pereira',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'daniel.serezane@unesp.br',
+        name: 'Guilherme Eduardo Silva Batalhoti',
+        description: ' ',
+        email: 'guilherme.batalhoti@unesp.br',
       },
       {
-        name: 'Gustavo Becelli do Nacimento',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'gustavo.becelli@unesp.br',
+        name: 'Lucas Vinícius Voltera',
+        description: ' ',
+        email: 'lucas.voltera@unesp.br',
       },
-    ],
-  ),
-  createEvent(
-    'GENERIC-TITLE-AUTOMATED-TESTING',
-    'Unknown',
-    0,
-    new Date('1970-01-01T00:00'),
-    60,
-    'Este minicurso irá apresentar os principais conceitos e práticas '
-    + 'sobre a escrita de testes automatizados de software, abordando '
-    + 'suas vantagens e desafios.',
-    'Abordagem da prática de testes automatizados de software..',
-    [
-      {
-        name: 'Thiago Aires Geraldo de Almeida',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: 'thiago.almeida@lambda3.com.br',
-      },
-    ],
+    ]
   ),
   createEvent(
     'Criando modelo de ML de maneira colaborativa - Uma aplicação com MLflow',
-    'Uknown',
+    'Laboratório central 6B',
     0,
     new Date('2022-10-20T08:00'),
     240,
-    'Demonstrar o uso colaborativo do MLflow para implantar modelos de '
-    + 'Machine Learning com Databricks.',
+    'Demonstrar o uso colaborativo do MLflow para implantar modelos de ' + 'Machine Learning com Databricks.',
     'Implantação de modelos de Machine Learning com Databricks.',
     [
       {
         name: 'Téo Calvo',
-        description: '...',
-        link: { path: 'https://github.com/', label: 'GitHub', },
+        description: ' ',
         email: 'teo.bcalvo@gmail.com',
       },
-    ],
+    ]
   ),
   createEvent(
-    'Sistemas Conversacionais: Oportunidades e Desafios',
-    'Unknown',
-    1,
-    new Date('2022-10-17T20:00'),
+    'Testes automatizados de software: vantagens, desafios e técnicas de escrita',
+    'Laboratório central 6B',
+    0,
+    new Date('1970-01-01T00:00'),
     60,
-    'Esta palestra abordará alguns dos avanços atuais no design e '
-    + 'desenvolvimento de sistemas conversacionais, tanto quanto '
-    + 'resultados recentes da pesquisa de Claudio.',
-    'Abordagem de sistemas conversacionais após uma década de progresso.',
+    'Este minicurso irá apresentar os principais conceitos e práticas ' +
+      'sobre a escrita de testes automatizados de software, abordando ' +
+      'suas vantagens e desafios.',
+    'Abordagem da prática de testes automatizados de software..',
     [
       {
-        name: 'Claudio Pinhanez',
-        description:
-          'Pesquisador da IBM Research, vice-diretor do C4AI (Centro de '
-          + 'Pesquisas em Inteligência Artificial), especialista em inteligência '
-          + 'artificial, interação homem-máquina, sistemas conversacionais '
-          + 'e ciência de serviços.',
-        link: { path: 'https://github.com/', label: 'GitHub', },
-        email: '...',
+        name: 'Thiago Aires Geraldo de Almeida',
+        description: ' ',
+        email: 'thiago.almeida@lambda3.com.br',
       },
-    ],
+    ]
+  ),
+  createEvent(
+    'Desenvolvimento fullstack web com ReactJs e Express',
+    'Laboratório central 6B',
+    0,
+    new Date('2022-10-17T14:00'),
+    60,
+    'Este minicurso irá apresentar os principais conceitos e práticas ' +
+      'sobre a escrita de testes automatizados de software, abordando ' +
+      'suas vantagens e desafios.',
+    'Abordagem da prática de testes automatizados de software..',
+    [
+      {
+        name: 'João Vitor Couto Oliveira',
+        description: ' ',
+        email: 'joao.couto@unesp.br',
+      },
+      {
+        name: 'Rafael Nogueira Correia',
+        description: ' ',
+        email: 'rafael.n.correia@unesp.br',
+      },
+    ]
+  ),
+  createEvent(
+    'Introdução ao ReactJs',
+    'Laboratório central 6B',
+    0,
+    new Date('2022-10-19T14:00'),
+    60,
+    'Abordagem prática no desenvolvimento de interfaces',
+    'Abordagem prática no desenvolvimento de interfaces',
+    [
+      {
+        name: 'Gabriel Cecon Carlsen',
+        description: ' ',
+        email: 'cecon.carlsen@unesp.br',
+      },
+      {
+        name: 'Guilherme de Aguiar Pacianotto',
+        description: ' ',
+        email: 'g.pacianotto@unesp.br',
+      },
+    ]
+  ),
+  createEvent(
+    'Introdução ao ReactJs',
+    'Laboratório central 6B',
+    0,
+    new Date('2022-10-20T14:00'),
+    60,
+    'Abordagem prática no desenvolvimento de interfaces',
+    'Abordagem prática no desenvolvimento de interfaces',
+    [
+      {
+        name: 'Gabriel Cecon Carlsen',
+        description: ' ',
+        email: 'cecon.carlsen@unesp.br',
+      },
+      {
+        name: 'Guilherme de Aguiar Pacianotto',
+        description: ' ',
+        email: 'g.pacianotto@unesp.br',
+      },
+    ]
   ),
 ];
 
