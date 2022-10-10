58fc5000f37ca1f963b2e3ccbdda81ab9ede8676
Yudi <renanyaguinuma@hotmail.com>
2022-10-10 11:23:48 -0300

    Add line breaks

--- src/lib/shared/ScheduleData5B.ts
+++ src/lib/shared/ScheduleData5B.ts
@@ -8,7 +8,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-20T08:00'),
     240,
-    'Apresentar os principais conceitos sobre arquitetura multicamadas e os benefícios de Clean Architecture, como também desmistificar a complexidade da arquitetura proposta por Robert C. Martin. Pré-requisitos: conceitos básicos de lógica e desenvolvimento de software, conceitos de programação orientada a objetos, desejável: conhecimento básico em Java. Materiais necessários: nenhum.',
+    'Apresentar os principais conceitos sobre arquitetura multicamadas e os benefícios de Clean Architecture, como também desmistificar a complexidade da arquitetura proposta por Robert C. Martin.\n\nPré-requisitos: conceitos básicos de lógica e desenvolvimento de software, conceitos de programação orientada a objetos, desejável: conhecimento básico em Java.\n\nMateriais necessários: nenhum.',
     'Programação utilizando Clean Architecture',
     [
       {
@@ -24,8 +24,8 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-17T14:00'),
     240,
-    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto. Pré-requisitos: Conhecimentos básicos de programação: variáveis, constantes, tipos de dados, laços, desvios condicionais, funções e arranjos. É recomendável (não obrigatório) ter noções de structs, classes e pattern matching. Materiais Necessários: Computador com a linguagem Rust (https://www.rust-lang.org/pt-BR/learn/get-started) instalada. Editor de texto/ambiente de programação, preferencialmente Visual Studio Code.',
-    'Introdução a linguagem Rust',
+    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto. \n\nPré-requisitos: Conhecimentos básicos de programação: variáveis, constantes, tipos de dados, laços, desvios condicionais, funções e arranjos. É recomendável (não obrigatório) ter noções de structs, classes e pattern matching. \n\nMateriais necessários: Computador com a linguagem Rust (https://www.rust-lang.org/pt-BR/learn/get-started) instalada. Editor de texto/ambiente de programação, preferencialmente Visual Studio Code.',
+    'Introdução à linguagem Rust',
     [
       {
         name: 'Daniel Henrique Serezane Pereira',
@@ -47,7 +47,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-18T14:00'),
     120,
-    'Este minicurso irá ensinar utilizar o framework CSS Bootstrap. Pré-requisitos: conhecimento básico em HTML e CSS. Materiais Necessários: Visual Studio Code',
+    'Este minicurso irá ensinar utilizar o framework CSS Bootstrap. \n\nPré-requisitos: conhecimento básico em HTML e CSS. \n\nMateriais necessários: Visual Studio Code',
     'Introdução ao Bootstrap',
     [
       {
@@ -70,8 +70,8 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-19T14:00'),
     240,
-    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto. Pré-requisitos: Conhecimentos básicos de programação: variáveis, constantes, tipos de dados, laços, desvios condicionais, funções e arranjos. É recomendável (não obrigatório) ter noções de structs, classes e pattern matching. Materiais Necessários: Computador com a linguagem Rust (https://www.rust-lang.org/pt-BR/learn/get-started) instalada. Editor de texto/ambiente de programação, preferencialmente Visual Studio Code.',
-    'Introdução a linguagem Rust',
+    'Apresentar a linguagem Rust, o contexto na qual é utilizada (principais aplicações no mercado) e o porquê de ser uma solução preferível em detrimento de outras, apoiando-se na criação de um projeto que exemplifique os diferenciais da linguagem. Durante a apresentação da linguagem, será demonstrada sua sintaxe, principais instruções, paradigmas de programação e o restante necessário para a construção do projeto. \n\nPré-requisitos: Conhecimentos básicos de programação: variáveis, constantes, tipos de dados, laços, desvios condicionais, funções e arranjos. É recomendável (não obrigatório) ter noções de structs, classes e pattern matching. \n\nMateriais necessários: Computador com a linguagem Rust (https://www.rust-lang.org/pt-BR/learn/get-started) instalada. Editor de texto/ambiente de programação, preferencialmente Visual Studio Code.',
+    'Introdução à linguagem Rust',
     [
       {
         name: 'Daniel Henrique Serezane Pereira',
@@ -93,7 +93,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-20T14:00'),
     120,
-    'Este minicurso irá ensinar utilizar o framework CSS Bootstrap. Pré-requisitos: conhecimento básico em HTML e CSS. Materiais Necessários: Visual Studio Code.',
+    'Este minicurso irá ensinar utilizar o framework CSS Bootstrap. \n\nPré-requisitos: conhecimento básico em HTML e CSS. \n\nMateriais necessários: Visual Studio Code.',
     'Introdução ao Bootstrap',
     [
       {
@@ -111,13 +111,13 @@ const ScheduleData: ScheduleEventList = [
     ]
   ),
   createEvent(
-    'Introdução a gitHub',
+    'Introdução ao GitHub',
     'Laboratório central 5B',
     0,
     new Date('2022-10-21T14:00'),
     150,
-    'Introdução ao básico sobre o gitHub e seus recursos. Pré-requisitos: Nenhum. Materiais Necessários: Git instalado na máquina, Visual Studio Code ou outro editor de sua escolha.',
-    'Introdução a plataforma GitHub',
+    'Introdução ao básico sobre o GitHub e seus recursos. \n\nPré-requisitos: Nenhum. \n\nMateriais necessários: Git instalado na máquina, Visual Studio Code ou outro editor de sua escolha.',
+    'Introdução à plataforma GitHub',
     [
       {
         name: 'Vinicius Polachini Mayer Freitas De Jesus',
@@ -139,7 +139,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-18T16:00'),
     120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa. Pré-requisito: conhecimento em inglês. Materiais necessários: computador com acesso à internet.',
+    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa. Pré-requisito: conhecimento em inglês. \n\nMateriais necessários: computador com acesso à internet.',
     'Desenvolvimento de résumé e prática de entrevista.',
     [
       {
@@ -162,7 +162,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-20T16:00'),
     120,
-    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa. Pré-requisito: conhecimento em inglês. Materiais necessários: computador com acesso à internet.',
+    'Este minicurso irá ensinar como escrever um resumo curto (résumé) e se preparar para entrevistas em língua inglesa. Pré-requisito: conhecimento em inglês. \n\nMateriais necessários: computador com acesso à internet.',
     'Desenvolvimento de résumé e prática de entrevista.',
     [
       {
--- src/lib/shared/ScheduleData8B.ts
+++ src/lib/shared/ScheduleData8B.ts
@@ -8,7 +8,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-17T08:00'),
     240,
-    'Introduzir o desenvolvimento de aplicativos mobile utilizando o framework cross-platform Flutter. Pré-requisitos: conhecimentos em programação orientado a objeto; noções básicas de métodos HTTP (GET, POST). Materiais Necessários: Android Studio, XCode (Apenas para Mac), Flutter, Java.',
+    'Introduzir o desenvolvimento de aplicativos mobile utilizando o framework cross-platform Flutter. \n\nPré-requisitos: conhecimentos em programação orientado a objeto; noções básicas de métodos HTTP (GET, POST). \n\nMateriais necessários: Android Studio, XCode (Apenas para Mac), Flutter, Java.',
     'Desenvolvimento de aplicações mobile',
     [
       {
@@ -25,7 +25,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-17T14:00'),
     240,
-    'Introduzir o desenvolvimento de aplicativos mobile utilizando o framework cross-platform Flutter. Pré-requisitos: conhecimentos em programação orientado a objeto; noções básicas de métodos HTTP (GET, POST). Materiais Necessários: Android Studio, XCode (Apenas para Mac), Flutter, Java.',
+    'Introduzir o desenvolvimento de aplicativos mobile utilizando o framework cross-platform Flutter. \n\nPré-requisitos: conhecimentos em programação orientado a objeto; noções básicas de métodos HTTP (GET, POST). \n\nMateriais necessários: Android Studio, XCode (Apenas para Mac), Flutter, Java.',
     'Desenvolvimento de aplicações mobile',
     [
       {
@@ -42,7 +42,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-18T14:00'),
     240,
-    'Introduzir alguns conceitos fundamentais do desenvolvimento web, com foco no frontend, e, desenvolver um portfólio simples, contendo os projetos hospedados no github do mesmo. Pré-requisitos: nenhum. Materiais Necessários: conta no GitHub e um editor de texto de sua preferência, sendo recomendado o Visual Studio Code ou Sublime Text.',
+    'Introduzir alguns conceitos fundamentais do desenvolvimento web, com foco no frontend, e, desenvolver um portfólio simples, contendo os projetos hospedados no github do mesmo. \n\nPré-requisitos: nenhum. \n\nMateriais necessários: conta no GitHub e um editor de texto de sua preferência, sendo recomendado o Visual Studio Code ou Sublime Text.',
     'Construção de um site em HTML e CSS básico.',
     [
       {
@@ -65,7 +65,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-19T14:00'),
     240,
-    'Introduzir alguns conceitos fundamentais do desenvolvimento web, com foco no frontend, e, desenvolver um portfólio simples, contendo os projetos hospedados no github do mesmo. Pré-requisitos: nenhum. Materiais Necessários: conta no GitHub e um editor de texto de sua preferência, sendo recomendado o Visual Studio Code ou Sublime Text.',
+    'Introduzir alguns conceitos fundamentais do desenvolvimento web, com foco no frontend, e, desenvolver um portfólio simples, contendo os projetos hospedados no github do mesmo. \n\nPré-requisitos: nenhum. \n\nMateriais necessários: conta no GitHub e um editor de texto de sua preferência, sendo recomendado o Visual Studio Code ou Sublime Text.',
     'Construção de um site em HTML e CSS básico.',
     [
       {
@@ -88,7 +88,7 @@ const ScheduleData: ScheduleEventList = [
     0,
     new Date('2022-10-20T14:00'),
     240,
-    'Apresentar algumas ferramentas que auxiliam no estudo diário da matemática de um curso de graduação em exatas, com o propósito de que o participante tenha conhecimento sobre a existência e certo domínio a respeito das mesmas. Pré-requisitos: noções básicas de fundamentos da Matemática. Materiais necessários: calculadora científica, caso possua.',
+    'Apresentar algumas ferramentas que auxiliam no estudo diário da matemática de um curso de graduação em exatas, com o propósito de que o participante tenha conhecimento sobre a existência e certo domínio a respeito das mesmas. \n\nPré-requisitos: noções básicas de fundamentos da Matemática. \n\nMateriais necessários: calculadora científica, caso possua.',
     'Como utilizar ferramentas que ajudam em exatas.',
     [
       {
