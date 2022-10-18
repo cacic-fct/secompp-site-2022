18f6f4d58665ce65752670630a999902526b5091
Willian Murayama <willianyos13@gmail.com>
2022-10-18 09:05:11 -0300

    Criação de seção para acessar os arquivos
    Arquivos que foram apresentados nos minicursos através do Google Drive

---
+++ src/routes/index-components/Schedule/Ending.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { Button } from 'sveltestrap';
+import '$lib/styles/global.scss';
+</script>
+
+    <div class="text-center intro-text">
+        <hr class="my-5" />
+        <br>
+        O evento acabou, agradecemos aos que se inscreveram e participaram!<br>
+        Você pode acessar os materiais utilizados durante os minicursos aqui:<br>
+        <Button outline class="my-3" size="lg" href="https://fct-pp.web.app/eventos">Acessar o material</Button>
+    </div>
