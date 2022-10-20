0a10079bc889203e1568d6912d9dbe70dbcebb83
Willian Murayama <willianyos13@gmail.com>
2022-10-20 17:03:55 -0300

    Modificação de formatação

--- src/routes/index-components/Schedule/Ending.svelte
+++ src/routes/index-components/Schedule/Ending.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-import { Button } from 'sveltestrap';
-import '$lib/styles/global.scss';
+  import { Button } from 'sveltestrap';
+  import '$lib/styles/global.scss';
 </script>
-<div class="container"><div class="text-center intro-text">
+
+<div class="container">
+  <div class="text-center intro-text">
     <hr class="my-5" />
-    <br>
-    O evento acabou, agradecemos aos que se inscreveram e participaram!<br>
-    Você pode acessar os materiais utilizados durante os minicursos aqui:<br>
-    <Button outline class="my-3" size="lg" href="https://drive.google.com/drive/folders/1rmuhUMoa3cWbGFHj3OxrTUepA_1_3vrc?usp=sharing">Acessar o material</Button>
-</div></div>
+    <br />
+    O evento acabou, agradecemos aos que se inscreveram e participaram!<br />
+    Você pode acessar os materiais utilizados durante os minicursos aqui:<br />
+    <Button
+      outline
+      class="my-3"
+      size="lg"
+      href="https://drive.google.com/drive/folders/1rmuhUMoa3cWbGFHj3OxrTUepA_1_3vrc?usp=sharing"
+      >Acessar o material</Button>
+  </div>
+</div>
